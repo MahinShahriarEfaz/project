40002081183f1fcb2921852e21c0f9e8e28f03be
MahinShahriarEfaz <mahin.shahriar.efaz@g.bracu.ac.bd>
2022-07-14 16:06:47 +0600

    3rd commit

--- efaz/src/App.js
+++ efaz/src/App.js
@@ -1,10 +1,21 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">PROJECT: Resell</a>
       </header>
-      <main>list OF products</main>
+      <main>
+        <h1>Featured Products</h1>
+        {data.products.map((products) => (
+          <div>
+            <img src={products.image} alt={products.name} />
+            <p>{products.name}</p>
+            <p>{products.price}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
