1e25a629e483c5813006e5f67cdcab04ee4003f3
MahinShahriarEfaz <mahin.shahriar.efaz@g.bracu.ac.bd>
2022-08-21 02:53:56 +0600

    commit

--- efaz/src/screens/HomeScreen.js
+++ efaz/src/screens/HomeScreen.js
@@ -6,6 +6,8 @@ import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -46,9 +48,9 @@ function HomeScreen() {
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
-          <div>Loading...</div>
+          <LoadingBox />
         ) : error ? (
-          <div>{error}</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row>
             {products.map((product) => (
